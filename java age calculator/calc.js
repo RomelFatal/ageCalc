500d03ccd266a43b0487dec3450556786a881f24
notcho3 <notcho3@gmail.com>
2019-07-09 13:13:29 -0400

    Add files via upload

---
+++ java age calculator/calc.js
@@ -0,0 +1,3 @@
+var age = prompt("What is your age?")
+var days = age * 365.25;//The 365.25 is for leap years
+alert(age + "" + " years is roughly  "  + days + " days"); //the "" in the middle are for spaces in the text
